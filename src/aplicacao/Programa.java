dbce6830a6e7209076e27f0d41305e3d1ea42470
souzaoe <souzaoe@hotmail.com>
2021-01-24 16:52:04 -0300

    Programa criado, origem e posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import tabuleiroDoJogo.Posicao;
+
 public class Programa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
-
 }
